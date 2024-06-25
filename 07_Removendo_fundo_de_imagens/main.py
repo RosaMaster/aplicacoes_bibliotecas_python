33946d4a1f739eb286bae20ccd50c492f01b0094
Eduardo Rosa <edu.wd2014@gmail.com>
2024-06-25 00:44:58 -0300

    feat: updated 08

--- 07_Removendo_fundo_de_imagens/main.py
+++ 07_Removendo_fundo_de_imagens/main.py
@@ -1,14 +1,16 @@
-from rembg import remove
+from rembg import remove 
 from PIL import Image
+from DATA.lista_imagem import lista_imagem
 
-# Importa a imagem original
-img = Image.open('img/ds.jpeg')
-                 
-#Remove o fundo da imagem
-img_without_back = remove(img)
+# Remove o fundo de todas as imagens
+for i in range(len(lista_imagem)):
 
-# Salva a imagem sem fundo
-img_without_back.save('ds_without_back.png')
+    # Abre a imagem
+    img = Image.open(f'img/{lista_imagem[i]}')
 
-print("Imagem salva com sucesso!")
+    # Remove o fundo da imagem
+    img_without_back = remove(img)
+
+    # Salva a imagem sem fundo
+    img_without_back.save(f'img/img_0{i + 1}_without_back.jpg')
 
